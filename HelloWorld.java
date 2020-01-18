9ce56c9f103e031aba2dc9bd3266b1c08534cc78
iverson790819 <iverson790819@gmail.com>
2020-01-18 13:55:03 +0800

    add one line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("­ü­ò!¤£¿ù³á!");
+        System.out.println("ï¿½ï¿½ï¿½ï¿½!ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½!");
+		System.out.println("ï¿½ï¿½ï¿½ï¿½!ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½!");
     }
 
 
