879f57ec86848807324b42d29018e8656f2e0fc7
iverson790819 <iverson790819@gmail.com>
2020-01-18 15:02:28 +0800

    java master

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("����!������!");
+        System.out.println("JAVA MASTER!!");
 		System.out.println("����!������!");
     }
 
